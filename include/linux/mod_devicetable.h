80c2c9fb00d76e567b411cc983595cc04fbe136a
Xu Yilun <yilun.xu@intel.com>
2020-03-12 15:36:57 +0800

    dfl: move dfl_device_id to mod_devicetable.h
    In order to support MODULE_DEVICE_TABLE() for dfl device driver, this
    patch moves struct dfl_device_id to mod_devicetable.h
    
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>
    Signed-off-by: Wu Hao <hao.wu@intel.com>
    Signed-off-by: Matthew Gerlach <matthew.gerlach@linux.intel.com>
    Signed-off-by: Russ Weight <russell.h.weight@intel.com>
    ----
    v4: rename dflsub bus to dfl bus

--- include/linux/mod_devicetable.h
+++ include/linux/mod_devicetable.h
@@ -836,4 +836,16 @@ struct mhi_device_id {
 	kernel_ulong_t driver_data;
 };
 
+/**
+ * struct dfl_device_id -  dfl device identifier
+ * @type: type of DFL FIU of the device. 0 for FME, 1 for PORT
+ * @feature_id: 64 bits feature identifier local to its DFL FIU type.
+ * @driver_data: Driver specific data
+ */
+struct dfl_device_id {
+	unsigned int type;
+	unsigned long long feature_id;
+	kernel_ulong_t driver_data;
+};
+
 #endif /* LINUX_MOD_DEVICETABLE_H */
