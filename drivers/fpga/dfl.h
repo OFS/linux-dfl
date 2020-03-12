dd398fb3e8aacdf2a32383d0e8030a6728704017
Xu Yilun <yilun.xu@intel.com>
2020-03-12 15:36:57 +0800

    dfl: move dfl_device_id to mod_devicetable.h
    In order to support MODULE_DEVICE_TABLE() for dfl device driver, this
    patch moves struct dfl_device_id to mod_devicetable.h
    
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>
    Signed-off-by: Wu Hao <hao.wu@intel.com>
    Signed-off-by: Matthew Gerlach <matthew.gerlach@linux.intel.com>
    Signed-off-by: Russ Weight <russell.h.weight@intel.com>
    ----
    v4: rename dflsub bus to dfl bus

--- drivers/fpga/dfl.h
+++ drivers/fpga/dfl.h
@@ -26,6 +26,7 @@
 #include <linux/slab.h>
 #include <linux/uuid.h>
 #include <linux/fpga/fpga-region.h>
+#include <linux/mod_devicetable.h>
 
 /* maximum supported number of ports */
 #define MAX_DFL_FPGA_PORT_NUM 4
@@ -533,18 +534,6 @@ enum dfl_id_type {
 	FME_ID,
 	PORT_ID,
 	DFL_ID_MAX,
-};
-
-/**
- * struct dfl_device_id -  dfl device identifier
- * @type: Type of DFL FIU of the device. See enum dfl_id_type.
- * @feature_id: 64 bits feature identifier local to its DFL FIU type.
- * @driver_data: Driver specific data
- */
-struct dfl_device_id {
-	unsigned int type;
-	unsigned long long feature_id;
-	unsigned long driver_data;
 };
 
 /**
