051acf261d7cfe431269b0666ba86a0ff3f5cc3b
Xu Yilun <yilun.xu@intel.com>
2020-03-23 15:13:45 +0800

    ethernet: dfl-eth-group: add DFL eth group private feature driver
    This driver supports the DFL Ether Group private feature for the Intel(R)
    PAC N3000 FPGA Smart NIC.
    
    The DFL Ether Group private feature contains several ether interfaces,
    each of them contains an Ether Wrapper and several ports (phy-mac pair).
    There are two types of interfaces, line side & host side. These 2 types of
    interfaces, together with FPGA internal logic, act as several independent
    pipes between the host Ethernet Controller and the front-panel cages. The
    FPGA logic between 2 interfaces implements user defined mac layer
    offloading.
    
    The line side ether interfaces connect to the Parkvale serdes transceiver
    chips, which are working in 4 ports 10G/25G retimer mode.
    
    There are several configurations (8x10G, 2x1x25G, 2x2x25G ...) for
    different connections between line side and host side. Not all ports are
    active in some configurations.
    
    For the line side port, the driver connects to the Parkvale phy device
    and then register net device for each active port.
    
    For the host side port, its link state is always on. The host side port
    always has same features as host side ether controller, so there is no
    need to register a netdev for it. The driver just enables the port on
    probe.
    
    This patch supports the 25G configurations. Support for 10G will be in
    other patches.
    
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>
    Signed-off-by: Russ Weight <russell.h.weight@intel.com>
    ----
    v2: no change
    v3: Add the Documentation for pause_quanta & holdoff.
        Some minor fix
    v4: initialize ret local variable
        check netdev for NULL
        ensure null termination of edev->phy_id
        declare static for some functions
        remove phydev field in struct n3000_net_priv
        fix kernel warning on driver remove

---
+++ drivers/net/ethernet/intel/dfl-eth-group.h
@@ -0,0 +1,83 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/* Internal header file for FPGA DFL Ether Group Driver
+ *
+ * Copyright (C) 2020 Intel Corporation. All rights reserved.
+ */
+
+#ifndef __DFL_ETH_GROUP_H__
+#define __DFL_ETH_GROUP_H__
+
+#include <linux/netdevice.h>
+#include <linux/phy.h>
+#include <linux/rtnetlink.h>
+
+/* Used when trying to find a virtual mii bus on a specific dfl device.
+ * dev_name(dfl base device)-mii
+ */
+#define DFL_ETH_MII_ID_FMT "%s-mii"
+
+struct eth_dev {
+	struct dfl_eth_group *egroup;
+	struct device *dev;
+	int index;
+	bool lw_mac;
+	struct eth_com *phy;
+	struct eth_com *mac;
+	struct net_device *netdev;
+
+	char phy_id[MII_BUS_ID_SIZE + 3];
+};
+
+struct eth_dev_ops {
+	int (*lineside_init)(struct eth_dev *edev);
+	void (*lineside_remove)(struct eth_dev *edev);
+	int (*reset)(struct eth_dev *edev, bool en);
+};
+
+struct n3000_net_priv {
+	struct eth_dev *edev;
+};
+
+static inline struct eth_dev *net_device_to_eth_dev(struct net_device *netdev)
+{
+	struct n3000_net_priv *priv = netdev_priv(netdev);
+
+	return priv->edev;
+}
+
+struct stat_info {
+	unsigned int addr;
+	char string[ETH_GSTRING_LEN];
+};
+
+#define STAT_INFO(_addr, _string) \
+	.addr = _addr, .string = _string,
+
+int do_eth_com_write_reg(struct eth_com *ecom, bool add_feature,
+			 u16 addr, u32 data);
+int do_eth_com_read_reg(struct eth_com *ecom, bool add_feature,
+			u16 addr, u32 *data);
+
+#define eth_com_write_reg(ecom, addr, data)	\
+	do_eth_com_write_reg(ecom, false, addr, data)
+
+#define eth_com_read_reg(ecom, addr, data)	\
+	do_eth_com_read_reg(ecom, false, addr, data)
+
+#define eth_com_add_feat_write_reg(ecom, addr, data)	\
+	do_eth_com_write_reg(ecom, true, addr, data)
+
+#define eth_com_add_feat_read_reg(ecom, addr, data)	\
+	do_eth_com_read_reg(ecom, true, addr, data)
+
+u64 read_mac_stats(struct eth_com *ecom, unsigned int addr);
+
+struct net_device *n3000_netdev_create(struct eth_dev *edev);
+netdev_tx_t n3000_dummy_netdev_xmit(struct sk_buff *skb,
+				    struct net_device *dev);
+
+extern struct eth_dev_ops dfl_eth_dev_25g_ops;
+extern struct eth_dev_ops dfl_eth_dev_40g_ops;
+
+#endif /* __DFL_ETH_GROUP_H__ */
