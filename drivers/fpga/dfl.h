63a863bea86382a2ff03ccf99a0c00bf65676b71
Xu Yilun <yilun.xu@intel.com>
2020-04-21 18:08:50 +0800

    fpga: dfl: move dfl-bus related APIs to include/linux/fpga/dfl-bus.h
    The patch moves dfl-bus related APIs to include/linux/fpga/dfl-bus.h
    
    Now the DFL sub feature drivers could be made as independent modules and
    put in different folders according to their functionality. In order for
    scattered sub feature drivers to include dfl bus APIs, move the dfl bus
    APIs to a new header file in the public folder.
    
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>
    Reviewed-by: Wu Hao <hao.wu@intel.com>
    Reviewed-by: Russ Weight <russell.h.weight@intel.com>

--- drivers/fpga/dfl.h
+++ drivers/fpga/dfl.h
@@ -26,7 +26,6 @@
 #include <linux/slab.h>
 #include <linux/uuid.h>
 #include <linux/fpga/fpga-region.h>
-#include <linux/mod_devicetable.h>
 
 /* maximum supported number of ports */
 #define MAX_DFL_FPGA_PORT_NUM 4
@@ -526,73 +525,5 @@ long dfl_feature_ioctl_get_num_irqs(struct platform_device *pdev,
 long dfl_feature_ioctl_set_irq(struct platform_device *pdev,
 			       struct dfl_feature *feature,
 			       unsigned long arg);
-
-/**
- * enum dfl_id_type - define the DFL FIU types
- */
-enum dfl_id_type {
-	FME_ID,
-	PORT_ID,
-	DFL_ID_MAX,
-};
-
-/**
- * struct dfl_device - represent an dfl device on dfl bus
- *
- * @dev: Generic device interface.
- * @type: Type of DFL FIU of the device. See enum dfl_id_type.
- * @feature_id: 64 bits feature identifier local to its DFL FIU type.
- * @mmio_res: MMIO resource of this dfl device.
- * @irqs: List of Linux IRQ numbers of this dfl device.
- * @num_irqs: number of IRQs supported by this dfl device.
- * @cdev: pointer to DFL FPGA container device this dfl device belongs to.
- * @id_entry: matched id entry in dfl driver's id table.
- */
-struct dfl_device {
-	struct device dev;
-	unsigned int type;
-	unsigned long long feature_id;
-	struct resource mmio_res;
-	int *irqs;
-	unsigned int num_irqs;
-	struct dfl_fpga_cdev *cdev;
-	const struct dfl_device_id *id_entry;
-};
-
-/**
- * struct dfl_driver - represent an dfl device driver
- *
- * @drv: Driver model structure.
- * @id_table: Pointer to table of device IDs the driver is interested in.
- * @probe: Callback for device binding.
- * @remove: Callback for device unbinding.
- */
-struct dfl_driver {
-	struct device_driver drv;
-	const struct dfl_device_id *id_table;
-
-	int (*probe)(struct dfl_device *dfl_dev);
-	int (*remove)(struct dfl_device *dfl_dev);
-};
-
-#define to_dfl_dev(d) container_of(d, struct dfl_device, dev)
-#define to_dfl_drv(d) container_of(d, struct dfl_driver, drv)
-
-/*
- * use a macro to avoid include chaining to get THIS_MODULE
- */
-#define dfl_driver_register(drv) \
-	__dfl_driver_register(drv, THIS_MODULE)
-int __dfl_driver_register(struct dfl_driver *dfl_drv, struct module *owner);
-void dfl_driver_unregister(struct dfl_driver *dfl_drv);
-
-/* module_dfl_driver() - Helper macro for drivers that don't do
- * anything special in module init/exit.  This eliminates a lot of
- * boilerplate.  Each module may only use this macro once, and
- * calling it replaces module_init() and module_exit()
- */
-#define module_dfl_driver(__dfl_driver) \
-	module_driver(__dfl_driver, dfl_driver_register, \
-		      dfl_driver_unregister)
 
 #endif /* __FPGA_DFL_H */
