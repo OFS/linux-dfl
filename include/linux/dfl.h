c96948f848a2a036c6decdda373f0dde42f71191
Xu Yilun <yilun.xu@intel.com>
2020-04-21 22:51:00 +0800

    fpga: dfl: add an API to get the base device for dfl device
    This patch adds an API for dfl devices to find which physical device
    owns the DFL.
    
    This patch makes preparation for supporting DFL Ether Group private
    feature driver. It uses this information to determine which retimer
    device physically connects to which ether group.
    
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>
    Reviewed-by: Matthew Gerlach <matthew.gerlach@linux.intel.com>
    ----
    v2: change commit msg according to Matthew's suggestion.
    v3: no change.

--- include/linux/dfl.h
+++ include/linux/dfl.h
@@ -67,6 +67,8 @@ struct dfl_driver {
 #define to_dfl_dev(d) container_of(d, struct dfl_device, dev)
 #define to_dfl_drv(d) container_of(d, struct dfl_driver, drv)
 
+struct device *dfl_dev_get_base_dev(struct dfl_device *dfl_dev);
+
 /*
  * use a macro to avoid include chaining to get THIS_MODULE.
  */
