b572f142fe05f4c49ac76ccbdf8f6b67f9950160
Michael Adler <Michael.Adler@intel.com>
2020-05-07 19:30:56 +0800

    fpga: dfl: afu: add read-only/write-only dma mapping support
    This patch adds support for Read-only and Write-only DMA mapping
    through existing DMA_MAP/UNMAP ioctl interface, it introduces
    two new flags DFL_DMA_MAP_FLAG_READ (mem to dev) and
    DFL_DMA_MAP_FLAG_WRITE (dev to mem).
    
    Signed-off-by: Michael Adler <Michael.Adler@intel.com>
    Signed-off-by: Wu Hao <hao.wu@intel.com>
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>

--- drivers/fpga/dfl-afu-main.c
+++ drivers/fpga/dfl-afu-main.c
@@ -711,6 +711,7 @@ static long afu_ioctl_get_region_info(struct dfl_feature_dev_data *fdata,
 static long
 afu_ioctl_dma_map(struct dfl_feature_dev_data *fdata, void __user *arg)
 {
+	u32 dma_mask = DFL_DMA_MAP_FLAG_READ | DFL_DMA_MAP_FLAG_WRITE;
 	struct dfl_fpga_port_dma_map map;
 	unsigned long minsz;
 	long ret;
@@ -720,10 +721,11 @@ afu_ioctl_dma_map(struct dfl_feature_dev_data *fdata, void __user *arg)
 	if (copy_from_user(&map, arg, minsz))
 		return -EFAULT;
 
-	if (map.argsz < minsz || map.flags)
+	if (map.argsz < minsz || map.flags & ~dma_mask)
 		return -EINVAL;
 
-	ret = afu_dma_map_region(fdata, map.user_addr, map.length, &map.iova);
+	ret = afu_dma_map_region(fdata, map.user_addr, map.length, map.flags,
+				 &map.iova);
 	if (ret)
 		return ret;
 
