f62367c091f3cbccb09128c90230295f9d1f65e0
Tianfei zhang <tianfei.zhang@intel.com>
2021-03-08 07:05:11 -0500

    mfd: pmci: add flash ops
    Introducing fpga_flash_ops for read/write the flash for FPGA.
    1. write_blk: write the data into staging area in MAX10 BMC
    2. read_blk: read a block data from flash

--- drivers/mfd/intel-m10-bmc-pmci.c
+++ drivers/mfd/intel-m10-bmc-pmci.c
@@ -6,6 +6,7 @@
  *
  */
 
+#include <linux/bitfield.h>
 #include <linux/dfl.h>
 #include <linux/mfd/intel-m10-bmc.h>
 #include <linux/module.h>
@@ -13,11 +14,128 @@
 
 #define M10BMC_PMCI_INDIRECT_BASE 0x400
 
+#define PMCI_FLASH_CTRL 0x40
+#define PMCI_FLASH_WR_MODE BIT(0)
+#define PMCI_FLASH_RD_MODE BIT(1)
+#define PMCI_FLASH_BUSY    BIT(2)
+#define PMCI_FLASH_FIFO_SPACE GENMASK(13, 4)
+#define PMCI_FLASH_READ_COUNT GENMASK(25, 16)
+
+#define PMCI_FLASH_INT_US       1
+#define PMCI_FLASH_TIMEOUT_US   10000
+
+#define PMCI_FLASH_ADDR 0x44
+#define PMCI_FLASH_FIFO 0x800
+#define PMCI_READ_BLOCK_SIZE 0x800
+#define PMCI_FIFO_MAX_BYTES 0x800
+#define PMCI_FIFO_MAX_WORDS (PMCI_FIFO_MAX_BYTES / 4)
+
 struct pmci_device {
 	void __iomem *base;
 	struct device *dev;
 	struct intel_m10bmc m10bmc;
 };
+
+static void pmci_write_fifo(void __iomem *base, char *buf, size_t count)
+{
+       int i;
+       u32 val;
+
+       for (i = 0; i < count/4 ; i++) {
+               val = *(u32 *)(buf + i * 4);
+	       writel(val, base);
+       }
+}
+
+static void pmci_read_fifo(void __iomem *base, char *buf, size_t count)
+{
+       int i;
+       u32 val;
+
+       for (i = 0; i < count/4; i++) {
+               val = readl(base);
+               *(u32 *)(buf + i * 4) = val;
+       }
+}
+
+static u32
+pmci_get_write_space(struct pmci_device *pmci, u32 size)
+{
+	u32 count, val;
+	int ret;
+
+	ret = read_poll_timeout(readl, val,
+				FIELD_GET(PMCI_FLASH_FIFO_SPACE, val) ==
+				PMCI_FIFO_MAX_WORDS,
+				PMCI_FLASH_INT_US, PMCI_FLASH_TIMEOUT_US,
+				false, pmci->base + PMCI_FLASH_CTRL);
+	if (ret == -ETIMEDOUT)
+		return 0;
+
+	count = FIELD_GET(PMCI_FLASH_FIFO_SPACE, val) * 4;
+
+	return (size > count) ? count : size;
+}
+
+static int
+pmci_flash_bulk_write(struct intel_m10bmc *m10bmc, void *buf, u32 size)
+{
+	struct pmci_device *pmci = container_of(m10bmc, struct pmci_device, m10bmc);
+	u32 blk_size, n_offset = 0;
+
+	while (size) {
+		blk_size = pmci_get_write_space(pmci, size);
+		if (blk_size == 0) {
+			dev_err(pmci->dev, "get FIFO available size fail\n");
+			return -EIO;
+		}
+		size -= blk_size;
+		pmci_write_fifo(pmci->base + PMCI_FLASH_FIFO, buf + n_offset, blk_size);
+		n_offset += blk_size;
+	}
+
+	return 0;
+}
+
+static int
+pmci_flash_bulk_read(struct intel_m10bmc *m10bmc, void *buf,
+		     u32 addr, u32 size)
+{
+	struct pmci_device *pmci = container_of(m10bmc, struct pmci_device, m10bmc);
+	u32 blk_size, offset = 0, val;
+	int ret;
+
+	if (!IS_ALIGNED(addr, 4))
+		return -EINVAL;
+
+	while (size) {
+		blk_size = min_t(u32, size, PMCI_READ_BLOCK_SIZE);
+
+		writel(addr + offset, pmci->base + PMCI_FLASH_ADDR);
+
+		writel(FIELD_PREP(PMCI_FLASH_READ_COUNT, blk_size / 4)
+				| PMCI_FLASH_RD_MODE,
+			pmci->base + PMCI_FLASH_CTRL);
+
+		ret = readl_poll_timeout((pmci->base + PMCI_FLASH_CTRL), val,
+					 !(val & PMCI_FLASH_BUSY),
+					 PMCI_FLASH_INT_US, PMCI_FLASH_TIMEOUT_US);
+		if (ret) {
+			dev_err(pmci->dev, "%s timed out on reading flash 0x%xn",
+				__func__, val);
+			return ret;
+		}
+
+		pmci_read_fifo(pmci->base + PMCI_FLASH_FIFO, buf + offset, blk_size);
+
+		size -= blk_size;
+		offset += blk_size;
+
+		writel(0, pmci->base + PMCI_FLASH_CTRL);
+	}
+
+	return 0;
+}
 
 static const struct regmap_range m10bmc_pmci_regmap_range[] = {
 	regmap_reg_range(M10BMC_PMCI_SYS_BASE, M10BMC_PMCI_SYS_END),
@@ -39,6 +157,7 @@ static struct regmap_config m10bmc_pmci_regmap_config = {
 
 static int pmci_probe(struct dfl_device *ddev)
 {
+	struct fpga_flash_ops *pmci_flash_ops;
 	struct device *dev = &ddev->dev;
 	struct pmci_device *pmci;
 
@@ -46,9 +165,17 @@ static int pmci_probe(struct dfl_device *ddev)
 	if (!pmci)
 		return -ENOMEM;
 
+	pmci_flash_ops = devm_kzalloc(dev, sizeof(*pmci_flash_ops), GFP_KERNEL);
+	if (!pmci_flash_ops)
+		return -ENOMEM;
+
+	pmci_flash_ops->read_blk = pmci_flash_bulk_read;
+	pmci_flash_ops->write_blk = pmci_flash_bulk_write;
+
 	pmci->m10bmc.dev = dev;
 	pmci->dev = dev;
 	pmci->m10bmc.type = M10_N6000;
+	pmci->m10bmc.flash_ops = pmci_flash_ops;
 
 	pmci->base = devm_ioremap_resource(dev, &ddev->mmio_res);
 	if (IS_ERR(pmci->base))
