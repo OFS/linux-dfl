89bcf06a17185fbfe6d8c1c1e60ffb0ec4f1aa16
Russ Weight <russell.h.weight@intel.com>
2021-03-19 17:19:59 -0700

    fpga: sec-mgr: Use kmemdup_nul for sysfs input
    Switch from kstrndup() to kmemdup_nul() for sysfs input.
    
    Signed-off-by: Russ Weight <russell.h.weight@intel.com>

--- drivers/fpga/fpga-sec-mgr.c
+++ drivers/fpga/fpga-sec-mgr.c
@@ -275,7 +275,7 @@ static ssize_t filename_store(struct device *dev, struct device_attribute *attr,
 		goto unlock_exit;
 	}
 
-	smgr->filename = kstrndup(buf, count, GFP_KERNEL);
+	smgr->filename = kmemdup_nul(buf, count, GFP_KERNEL);
 	if (!smgr->filename) {
 		ret = -ENOMEM;
 		goto unlock_exit;
