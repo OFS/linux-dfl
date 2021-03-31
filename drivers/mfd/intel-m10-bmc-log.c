d6bc4ec7079165de958aab07f36ffcb9acf74dfb
Tianfei zhang <tianfei.zhang@intel.com>
2021-03-31 08:37:44 -0400

    mfd: intel-m10-bmc: expose bmc event log via nvmem
    Use the nvmem API to expose the event MAX10 BMC event logs to userspace.
    
    Signed-off-by: Tianfei Zhang <tianfei.zhang@intel.com>

--- drivers/mfd/intel-m10-bmc-log.c
+++ drivers/mfd/intel-m10-bmc-log.c
@@ -10,12 +10,17 @@
 #include <linux/mfd/intel-m10-bmc.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
+#include <linux/nvmem-provider.h>
+#include <linux/mod_devicetable.h>
 
 struct m10bmc_log {
 	struct device *dev;
 	struct intel_m10bmc *m10bmc;
 	unsigned int freq_s;		/* update frequency in seconds */
 	struct delayed_work dwork;
+	struct nvmem_device *bmc_event_log_nvmem;
+	struct nvmem_device *fpga_image_dir_nvmem;
+	struct nvmem_device *bom_info_nvmem;
 };
 
 #define M10BMC_TIMESTAMP_FREQ   60	/* 60 seconds between updates */
@@ -84,6 +89,72 @@ static struct attribute *m10bmc_log_attrs[] = {
 };
 ATTRIBUTE_GROUPS(m10bmc_log);
 
+static int bmc_nvmem_read(struct m10bmc_log *ddata, unsigned int addr,
+			  unsigned int off, void *val, size_t count)
+{
+	int ret;
+
+	if (!ddata->m10bmc->ops.flash_read)
+		return -ENODEV;
+
+	ret = ddata->m10bmc->ops.flash_read(ddata->m10bmc, val,
+					    addr + off, count);
+	if (ret) {
+		dev_err(ddata->dev, "failed to read flash %x\n", addr);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int bmc_event_log_nvmem_read(void *priv, unsigned int off,
+				    void *val, size_t count)
+{
+	struct m10bmc_log *ddata = priv;
+
+	return bmc_nvmem_read(ddata, PMCI_ERROR_LOG_ADDR, off, val, count);
+}
+
+static int fpga_image_dir_nvmem_read(void *priv, unsigned int off,
+				     void *val, size_t count)
+{
+	struct m10bmc_log *ddata = priv;
+
+	return bmc_nvmem_read(ddata, PMCI_FPGA_IMAGE_DIR_ADDR, off, val, count);
+}
+
+static int bom_info_nvmem_read(void *priv, unsigned int off,
+			       void *val, size_t count)
+{
+	struct m10bmc_log *ddata = priv;
+
+	return bmc_nvmem_read(ddata, PMCI_BOM_INFO_ADDR, off, val, count);
+}
+
+static struct nvmem_config bmc_event_log_nvmem_config = {
+	.name = "bmc_event_log",
+	.stride = 4,
+	.word_size = 1,
+	.size = PMCI_ERROR_LOG_SIZE,
+	.reg_read = bmc_event_log_nvmem_read,
+};
+
+static struct nvmem_config fpga_image_dir_nvmem_config = {
+	.name = "fpga_image_directory",
+	.stride = 4,
+	.word_size = 1,
+	.size = PMCI_FPGA_IMAGE_DIR_SIZE,
+	.reg_read = fpga_image_dir_nvmem_read,
+};
+
+static struct nvmem_config bom_info_nvmem_config = {
+	.name = "bom_info",
+	.stride = 4,
+	.word_size = 1,
+	.size = PMCI_BOM_INFO_SIZE,
+	.reg_read = bom_info_nvmem_read,
+};
+
 static int m10bmc_log_probe(struct platform_device *pdev)
 {
 	struct m10bmc_log *ddata;
@@ -100,6 +171,27 @@ static int m10bmc_log_probe(struct platform_device *pdev)
 
 	m10bmc_log_time_sync(&ddata->dwork.work);
 
+	bmc_event_log_nvmem_config.dev = ddata->dev;
+	bmc_event_log_nvmem_config.priv = ddata;
+
+	ddata->bmc_event_log_nvmem = devm_nvmem_register(ddata->dev, &bmc_event_log_nvmem_config);
+	if (IS_ERR(ddata->bmc_event_log_nvmem))
+		return PTR_ERR(ddata->bmc_event_log_nvmem);
+
+	fpga_image_dir_nvmem_config.dev = ddata->dev;
+	fpga_image_dir_nvmem_config.priv = ddata;
+
+	ddata->fpga_image_dir_nvmem = devm_nvmem_register(ddata->dev, &fpga_image_dir_nvmem_config);
+	if (IS_ERR(ddata->fpga_image_dir_nvmem))
+		return PTR_ERR(ddata->fpga_image_dir_nvmem);
+
+	bom_info_nvmem_config.dev = ddata->dev;
+	bom_info_nvmem_config.priv = ddata;
+
+	ddata->bom_info_nvmem = devm_nvmem_register(ddata->dev, &bom_info_nvmem_config);
+	if (IS_ERR(ddata->bom_info_nvmem))
+		return PTR_ERR(ddata->bom_info_nvmem);
+
 	return 0;
 }
 
@@ -111,6 +203,13 @@ static int m10bmc_log_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static const struct platform_device_id intel_m10bmc_log_ids[] = {
+	{
+		.name = "n6010bmc-log",
+	},
+	{ }
+};
+
 static struct platform_driver intel_m10bmc_log_driver = {
 	.probe = m10bmc_log_probe,
 	.remove = m10bmc_log_remove,
@@ -121,6 +220,7 @@ static struct platform_driver intel_m10bmc_log_driver = {
 };
 module_platform_driver(intel_m10bmc_log_driver);
 
+MODULE_DEVICE_TABLE(platform, intel_m10bmc_log_ids);
 MODULE_AUTHOR("Intel Corporation");
 MODULE_DESCRIPTION("Intel MAX10 BMC Log");
 MODULE_LICENSE("GPL v2");
