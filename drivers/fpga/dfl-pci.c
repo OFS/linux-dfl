6972a25977fa9ac88ca60fdb89c7bc631288f232
Matthew Gerlach <matthew.gerlach@linux.intel.com>
2021-06-02 11:47:14 -0700

    fpga: dfl: Allow for ports with no local bar space.
    From a fpga partial reconfiguration standpoint, a port
    may not be connected any local BAR space.  The port could
    be connected to a different PCIe Physical Function (PF) or
    Virtual Function (VF), in which case another driver instance
    would manage the endpoint.
    
    Signed-off-by: Matthew Gerlach <matthew.gerlach@linux.intel.com>

--- drivers/fpga/dfl-pci.c
+++ drivers/fpga/dfl-pci.c
@@ -245,6 +245,7 @@ static int find_dfls_by_default(struct pci_dev *pcidev,
 		v = readq(base + FME_HDR_CAP);
 		port_num = FIELD_GET(FME_CAP_NUM_PORTS, v);
 
+		dev_info(&pcidev->dev, "port_num = %d\n", port_num);
 		WARN_ON(port_num > MAX_DFL_FPGA_PORT_NUM);
 
 		for (i = 0; i < port_num; i++) {
@@ -260,6 +261,12 @@ static int find_dfls_by_default(struct pci_dev *pcidev,
 			 */
 			bar = FIELD_GET(FME_PORT_OFST_BAR_ID, v);
 			offset = FIELD_GET(FME_PORT_OFST_DFH_OFST, v);
+			if (bar >= PCI_STD_NUM_BARS) {
+				dev_info(&pcidev->dev, "skipping invalid port BAR %d\n", bar);
+				continue;
+			} else {
+				dev_info(&pcidev->dev, "BAR %d offset %u\n", bar, offset);
+			}
 			if (bars & BIT(bar)) {
 				dev_warn(&pcidev->dev, "skipping bad port BAR %d\n", bar);
 				continue;
