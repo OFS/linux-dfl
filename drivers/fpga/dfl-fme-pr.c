622031ea7a5917dcf81bc9f14319546b2043268b
Xu Yilun <yilun.xu@intel.com>
2021-06-24 13:34:30 +0800

    fpga: dfl: fme: fix kernel-doc comments for some functions
    lkp reported 2 build warnings:
    
       drivers/fpga/dfl/dfl-fme-pr.c:175: warning: Function parameter or member 'feature' not described in 'dfl_fme_create_mgr'
    
    >> drivers/fpga/dfl/dfl-fme-pr.c:280: warning: expecting prototype for
    >> dfl_fme_destroy_bridge(). Prototype was for dfl_fme_destroy_bridges()
    >> instead
    
    Fixes: 29de76240e86 ("fpga: dfl: fme: add partial reconfiguration sub feature support")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Xu Yilun <yilun.xu@intel.com>

--- drivers/fpga/dfl-fme-pr.c
+++ drivers/fpga/dfl-fme-pr.c
@@ -159,6 +159,7 @@ free_exit:
  * dfl_fme_create_mgr - create fpga mgr platform device as child device
  *
  * @pdata: fme platform_device's pdata
+ * @feature: the dfl fme PR sub feature
  *
  * Return: mgr platform device if successful, and error code otherwise.
  */
@@ -266,7 +267,7 @@ static void dfl_fme_destroy_bridge(struct dfl_fme_bridge *fme_br)
 }
 
 /**
- * dfl_fme_destroy_bridge - destroy all fpga bridge platform device
+ * dfl_fme_destroy_bridges - destroy all fpga bridge platform devices
  * @pdata: fme platform device's pdata
  */
 static void dfl_fme_destroy_bridges(struct dfl_feature_platform_data *pdata)
