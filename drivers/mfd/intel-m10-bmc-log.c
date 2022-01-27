7405e3d7fdcae98c79ced51b1ba1375f84557262
Matthew Gerlach <matthew.gerlach@linux.intel.com>
2022-01-26 16:36:35 -0800

    mfd: intel-m10-bmc-log: fix thread unsafe code
    A single, static variable was being modified and used
    by potentially multiple execution instances of code.
    
    Signed-off-by: Matthew Gerlach <matthew.gerlach@linux.intel.com>

--- drivers/mfd/intel-m10-bmc-log.c
+++ drivers/mfd/intel-m10-bmc-log.c
@@ -161,6 +161,7 @@ static struct nvmem_config bom_info_nvmem_config = {
 static int m10bmc_log_probe(struct platform_device *pdev)
 {
 	struct m10bmc_log *ddata;
+	struct nvmem_config nvconfig;
 
 	ddata = devm_kzalloc(&pdev->dev, sizeof(*ddata), GFP_KERNEL);
 	if (!ddata)
@@ -174,24 +175,27 @@ static int m10bmc_log_probe(struct platform_device *pdev)
 
 	m10bmc_log_time_sync(&ddata->dwork.work);
 
-	bmc_event_log_nvmem_config.dev = ddata->dev;
-	bmc_event_log_nvmem_config.priv = ddata;
+	memcpy(&nvconfig, &bmc_event_log_nvmem_config, sizeof(bmc_event_log_nvmem_config));
+	nvconfig.dev = ddata->dev;
+	nvconfig.priv = ddata;
 
-	ddata->bmc_event_log_nvmem = devm_nvmem_register(ddata->dev, &bmc_event_log_nvmem_config);
+	ddata->bmc_event_log_nvmem = devm_nvmem_register(ddata->dev, &nvconfig);
 	if (IS_ERR(ddata->bmc_event_log_nvmem))
 		return PTR_ERR(ddata->bmc_event_log_nvmem);
 
-	fpga_image_dir_nvmem_config.dev = ddata->dev;
-	fpga_image_dir_nvmem_config.priv = ddata;
+	memcpy(&nvconfig, &fpga_image_dir_nvmem_config, sizeof(fpga_image_dir_nvmem_config));
+	nvconfig.dev = ddata->dev;
+	nvconfig.priv = ddata;
 
-	ddata->fpga_image_dir_nvmem = devm_nvmem_register(ddata->dev, &fpga_image_dir_nvmem_config);
+	ddata->fpga_image_dir_nvmem = devm_nvmem_register(ddata->dev, &nvconfig);
 	if (IS_ERR(ddata->fpga_image_dir_nvmem))
 		return PTR_ERR(ddata->fpga_image_dir_nvmem);
 
-	bom_info_nvmem_config.dev = ddata->dev;
-	bom_info_nvmem_config.priv = ddata;
+	memcpy(&nvconfig, &bom_info_nvmem_config, sizeof(bom_info_nvmem_config));
+	nvconfig.dev = ddata->dev;
+	nvconfig.priv = ddata;
 
-	ddata->bom_info_nvmem = devm_nvmem_register(ddata->dev, &bom_info_nvmem_config);
+	ddata->bom_info_nvmem = devm_nvmem_register(ddata->dev, &nvconfig);
 	if (IS_ERR(ddata->bom_info_nvmem))
 		return PTR_ERR(ddata->bom_info_nvmem);
 
